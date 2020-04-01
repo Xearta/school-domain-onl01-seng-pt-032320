52d84858145ed20e3d641ce5eafd55c6fc03bbc7
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 03:28:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if not @roster.key?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+  def sort
+    @roster.collect do |grade, students|
+      students.sort!
+    end
+    @roster
+  end
+  
+end
