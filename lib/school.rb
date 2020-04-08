fe887ef6cc819775cbd23ad50d1052563ea0e6fb
Tanner Townsend <rsmcwow@gmail.com>
2020-04-08 20:04:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,16 +20,16 @@ class School
   end
   
   def sort
-<<<<<<< HEAD
+
     @roster.collect do |grade, students|
       students.sort!
     end
     @roster
-=======
+
     @roster.each do |grade, students|
       binding.pry
     end
->>>>>>> cecdea41b64980d6c632b5bd628b5e9ba48c1a22
+
   end
   
 end
